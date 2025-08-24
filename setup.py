16d51af811b095301dd2071452ca5a04571bed8a
wu <wu@.com>
2025-08-24 15:36:21 +0800

    savoirfairelinux/num2words/gpt-4o-6

--- setup.py
+++ setup.py
@@ -1,137 +1,79 @@
-import os
-import sys
-from shutil import rmtree
+# -*- coding: utf-8 -*-
+# Copyright (c) 2003, Taro Ogawa.  All Rights Reserved.
+# Copyright (c) 2013, Savoir-faire Linux inc.  All Rights Reserved.
 
-from setuptools import Command
-from setuptools import find_packages
-from setuptools import setup
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301 USA
 
-# Package meta-data.
-NAME = 'django-inlinecss'
-SRC_DIR = 'django_inlinecss'
-DESCRIPTION = 'A Django app useful for inlining CSS (primarily for e-mails)'
-URL = 'https://github.com/roverdotcom/django-inlinecss'
-EMAIL = 'philip@rover.com'
-AUTHOR = 'Philip Kimmey'
-REQUIRES_PYTHON = '>=3.8'
-VERSION = None
+import re
+from io import open
 
-# What packages are required for this module to be executed?
-REQUIRED = [
-    'Django>=3.2',
-    'pynliner',
-    'future>=0.16.0',
+from setuptools import find_packages, setup
+
+PACKAGE_NAME = "num2words"
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU Library or Lesser General Public License '
+    '(LGPL)',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Topic :: Software Development :: Internationalization',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Software Development :: Localization',
+    'Topic :: Text Processing :: Linguistic',
 ]
 
-# What packages are required only for tests?
-TESTS = [
-    'mock==5.1.0',
-    'pytest==8.0.1',
-    'pytest-django==4.8.0',
-]
-
-# What packages are optional?
-EXTRAS = {
-    'flake8': [
-        'flake8==7.0.0',
-        'flake8-isort==6.1.1',
-        'isort==5.13.2',
-        'testfixtures==8.0.0',
-    ],
-    "tests": TESTS,
-}
-
-here = os.path.abspath(os.path.dirname(__file__))
+LONG_DESC = open('README.rst', 'rt', encoding="utf-8").read() + '\n\n' + \
+            open('CHANGES.rst', 'rt', encoding="utf-8").read()
 
 
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
-
-try:
-    with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
-        long_description = "\n" + f.read()
-except FileNotFoundError:
-    long_description = DESCRIPTION
-
-# Load the package's __version__.py module as a dictionary.
-about = {}
-if not VERSION:
-    with open(os.path.join(here, SRC_DIR, "__version__.py")) as f:
-        exec(f.read(), about)
-else:
-    about["__version__"] = VERSION
-
-
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = "Build and publish the package."
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print(f"\033[1m{s}\033[0m")
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status("Removing previous builds…")
-            rmtree(os.path.join(here, "dist"))
-        except OSError:
-            pass
-
-        self.status("Building Source and Wheel (universal) distribution…")
-        os.system(f"{sys.executable} setup.py sdist bdist_wheel --universal")
-
-        self.status("Uploading the package to PyPI via Twine…")
-        os.system("twine upload dist/*")
-
-        self.status("Pushing git tags…")
-        os.system("git tag v{}".format(about["__version__"]))
-        os.system("git push --tags")
-
-        sys.exit()
+def find_version(fname):
+    """Parse file & return version number matching 0.0.1 regex
+    Returns str or raises RuntimeError
+    """
+    version = ''
+    with open(fname, 'r', encoding="utf-8") as fp:
+        reg = re.compile(r'__version__ = [\'"]([^\'"]*)[\'"]')
+        for line in fp:
+            m = reg.match(line)
+            if m:
+                version = m.group(1)
+                break
+    if not version:
+        raise RuntimeError('Cannot find version information')
+    return version
 
 
 setup(
-    name=NAME,
-    version=about["__version__"],
-    description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author=AUTHOR,
-    author_email=EMAIL,
-    license="MIT",
-    url=URL,
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    keywords=["html", "css", "inline", "style", "email"],
-    classifiers=[
-        "Environment :: Other Environment",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Topic :: Communications :: Email",
-        "Topic :: Text Processing :: Markup :: HTML",
-    ],
-    install_requires=REQUIRED,
-    tests_require=TESTS,
-    extras_require=EXTRAS,
-    # $ setup.py upload support.
-    cmdclass={
-        "upload": UploadCommand,
-    },
+    name=PACKAGE_NAME,
+    version=find_version("bin/num2words"),
+    description='Modules to convert numbers to words. Easily extensible.',
+    long_description=LONG_DESC,
+    license='LGPL',
+    author='Taro Ogawa <tso at users sourceforge net>',
+    author_email='tos@users.sourceforge.net',
+    maintainer='Savoir-faire Linux inc.',
+    maintainer_email='istvan.szalai@savoirfairelinux.com',
+    keywords=' number word numbers words convert conversion i18n '
+             'localisation localization internationalisation '
+             'internationalization',
+    url='https://github.com/savoirfairelinux/num2words',
+    packages=find_packages(exclude=['tests']),
+    test_suite='tests',
+    classifiers=CLASSIFIERS,
+    scripts=['bin/num2words'],
+    install_requires=["docopt>=0.6.2"],
+    tests_require=['delegator.py'],
 )
