59003b97f781278306d78125daa7fea8c0d6b539
wu <wu@.com>
2025-08-24 15:20:30 +0800

    domaintools/python_api/gpt-4o-6

--- setup.py
+++ setup.py
@@ -1,57 +1,111 @@
+#!/usr/bin/env python
+"""Defines the setup instructions for domaintools"""
+import glob
 import os
+import subprocess
+import sys
+from os import path
 
-from setuptools import find_packages, setup
+from setuptools import Extension, find_packages, setup
+from setuptools.command.test import test as TestCommand
 
-VERSION = __import__("import_export").__version__
+requires = ['requests']
+packages = ['domaintools']
 
-CLASSIFIERS = [
-    'Framework :: Django',
-    'Framework :: Django :: 2.2',
-    'Framework :: Django :: 3.1',
-    'Framework :: Django :: 3.2',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: BSD License',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3 :: Only',
-    'Topic :: Software Development',
-]
+major, minor, patch = sys.version_info[:3]
 
-install_requires = [
-    'diff-match-patch',
-    'Django>=2.2',
-    'tablib[html,ods,xls,xlsx,yaml]>=3.0.0',
-]
+if major >= 3:
+    if minor == 5 and patch >= 3:
+        packages.append('domaintools_async')
+        requires.append('aiohttp==3.4.4')
+    if minor > 5:
+        packages.append('domaintools_async')
+        requires.append('aiohttp>=3.6.0,<4.0.0')
+elif major == 2 and minor <= 6:
+    requires.extend(['ordereddict', 'argparse'])
+
+MYDIR = path.abspath(os.path.dirname(__file__))
+JYTHON = 'java' in sys.platform
+PYPY = bool(getattr(sys, 'pypy_version_info', False))
+CYTHON = False
+if not PYPY and not JYTHON:
+    try:
+        from Cython.Distutils import build_ext
+        CYTHON = True
+    except ImportError:
+        pass
+
+cmdclass = {}
+ext_modules = []
+if CYTHON:
+    def list_modules(dirname):
+        filenames = glob.glob(path.join(dirname, '*.py'))
+
+        module_names = []
+        for name in filenames:
+            module, ext = path.splitext(path.basename(name))
+            if module != '__init__':
+                module_names.append(module)
+
+        return module_names
+
+    ext_modules = [
+        Extension('domaintools.' + ext, [path.join('domaintools', ext + '.py')])
+        for ext in list_modules(path.join(MYDIR, 'domaintools'))]
+    cmdclass['build_ext'] = build_ext
 
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
-    readme = f.read()
+class PyTest(TestCommand):
+    extra_kwargs = {'tests_require': ['pytest', 'mock']}
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        sys.exit(pytest.main(self.test_args))
 
 
-setup(
-    name="django-import-export",
-    description="Django application and library for importing and exporting"
-                " data with included admin integration.",
-    long_description=readme,
-    version=VERSION,
-    author="Informatika Mihelac",
-    author_email="bmihelac@mihelac.org",
-    license='BSD License',
-    platforms=['OS Independent'],
-    url="https://github.com/django-import-export/django-import-export",
-    project_urls={
-        "Documentation": "https://django-import-export.readthedocs.io/en/stable/",
-        "Changelog": "https://django-import-export.readthedocs.io/en/stable/changelog.html",
-    },
-    packages=find_packages(exclude=["tests"]),
-    include_package_data=True,
-    install_requires=install_requires,
-    python_requires=">=3.6",
-    classifiers=CLASSIFIERS,
-    zip_safe=False,
-)
+cmdclass['test'] = PyTest
+
+try:
+   with open("README.md", "r") as f:
+       readme = f.read()
+except (IOError, ImportError, OSError, RuntimeError):
+   readme = ''
+
+setup(name='domaintools_api',
+      version='0.5.1',
+      description="DomainTools' Official Python API",
+      long_description=readme,
+      long_description_content_type="text/markdown",
+      author='DomainTools',
+      author_email='timothy@domaintools.com',
+      url='https://github.com/domaintools/python_api',
+      license="MIT",
+      entry_points={
+        'console_scripts': [
+            'domaintools = domaintools.cli:run',
+        ]
+      },
+      packages=packages,
+      install_requires=requires,
+      cmdclass=cmdclass,
+      ext_modules=ext_modules,
+      keywords='Python, Python3',
+      classifiers=['Development Status :: 6 - Mature',
+                   'Intended Audience :: Developers',
+                   'Natural Language :: English',
+                   'Environment :: Console',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.7',
+                   'Programming Language :: Python :: 3.8',
+                   'Topic :: Software Development :: Libraries',
+                   'Topic :: Utilities'],
+      **PyTest.extra_kwargs)
--- tests/settings.py
+++ tests/settings.py
@@ -1,111 +1,19 @@
-import os
-import sys
+"""Defines all test wide settings and variables"""
+from os import environ
 
-import django
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.sites',
-
-    'import_export',
-
-    'core',
-]
-
-SITE_ID = 1
-
-ROOT_URLCONF = "urls"
-
-DEBUG = True
-
-STATIC_URL = '/static/'
-
-SECRET_KEY = '2n6)=vnp8@bu0om9d05vwf7@=5vpn%)97-!d*t4zq1mku%0-@j'
-
-MIDDLEWARE = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': (
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.request',
-            ),
-        },
-    },
-]
-
-if django.VERSION >= (3, 2):
-    DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-if os.environ.get('IMPORT_EXPORT_TEST_TYPE') == 'mysql-innodb':
-    IMPORT_EXPORT_USE_TRANSACTIONS = True
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'import_export',
-            'USER': os.environ.get('IMPORT_EXPORT_MYSQL_USER', 'root'),
-            'PASSWORD': os.environ.get('IMPORT_EXPORT_MYSQL_PASSWORD', 'password'),
-            'HOST': '127.0.0.1',
-            'PORT': 3306,
-            'TEST': {
-                'CHARSET': 'utf8',
-                'COLLATION': 'utf8_general_ci',
-            }
-        }
-    }
+from domaintools import API
+from vcr import VCR
 
 
-elif os.environ.get('IMPORT_EXPORT_TEST_TYPE') == 'postgres':
-    IMPORT_EXPORT_USE_TRANSACTIONS = True
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'import_export',
-            'USER': os.environ.get('IMPORT_EXPORT_POSTGRESQL_USER'),
-            'PASSWORD': os.environ.get('IMPORT_EXPORT_POSTGRESQL_PASSWORD'),
-            'HOST': 'localhost',
-            'PORT': 5432
-        }
-    }
-else:
-    if 'test' in sys.argv:
-        database_name = ''
-    else:
-        database_name = os.path.join(os.path.dirname(__file__), 'database.db')
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': database_name,
-        }
-    }
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'handlers': {
-        'console': {
-            'class': 'logging.NullHandler'
-        }
-    },
-    'root': {
-        'handlers': ['console'],
-    }}
+def remove_server(response):
+    response.get('headers', {}).pop('server', None)
+    if 'url' in response:
+        response['url'] = response['url'].update_query(api_username='test', api_key='test')
+    return response
 
 
-USE_TZ = False
+vcr = VCR(before_record_response=remove_server, filter_query_parameters=['timestamp', 'signature', 'api_username'], filter_post_data_parameters=['timestamp', 'signature', 'api_username'],
+          cassette_library_dir='tests/fixtures/vcr/', path_transformer=VCR.ensure_suffix('.yaml'),
+          record_mode='new_episodes')
+with vcr.use_cassette('init_user_account'):
+    api = API(environ.get('TEST_USER', 'test'), environ.get('TEST_KEY', 'test'), always_sign_api_key=True)
